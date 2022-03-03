8459671b99aec6c903c22dd4fe7b1209db7aa34b
jaredbest <me@jaredbest.com>
2022-03-03 12:38:37 +0100

    Initial commit

---
+++ ASPDemoApplication/MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            // Force an error
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age;
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Corey", Age = 38 });
+            people.Add(new PersonModel { FirstName = "Joe", LastName = "Smith", Age = 27 });
+            people.Add(new PersonModel { FirstName = "Sarah", LastName = "Connor", Age = 42 });
+
+            return View(people);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Course Repo
+
+- **Title:** Introduction to ASP.NET MVC in C#: Basics, Advanced Topics, Tips, Tricks, Best Practices, and More
+- **Instructor:** Tim Corey
+- **Platform:** YouTube (Channel = IAmTimCorey)
+- **Note(s):**
